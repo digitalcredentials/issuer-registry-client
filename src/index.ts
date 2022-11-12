6848453e01e1e500c6d62e675d72f82ee736a402
Seth <sethduffin@gmail.com>
2022-11-12 11:33:31 -0700

    Add isomorphic-fetch polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /*!
  * Copyright (c) 2022 Digital Credentials Consortium. All rights reserved.
  */
+import 'isomorphic-fetch';
 
 export { registryCollections, loadRegistryCollections } from './registryCollections';
 export { generateIssuerAuthRegistry } from './issuerAuthRegistry';
