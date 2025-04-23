2f73599db30051fd54e900d9399bd848ee9c2d89
James Chartrand <jc.chartrand@gmail.com>
2025-04-23 12:14:19 -0400

    track and return list of unloaded registries

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@
  * {
  *   "type": "dcc-legacy",
  *   "name": "DCC Sandbox Registry",
- *   "url": "https://digitalcredentials.github.io/sandbox-registry/registry.json"
+ *   "url": "https://digitalcredentials.github.io/sandbox-registry/registry.json",
+ *   "checked": false
  * }
  * ```
  */
@@ -18,7 +19,8 @@ export interface Registry {
   type: 'oidf' | 'dcc-legacy'
   name: string
   url?: string
-  fetchEndpoint?: string
+  fetchEndpoint?: string,
+  checked?: boolean
 }
 
 /**
@@ -43,8 +45,13 @@ export interface IssuerMetaData {
 }
 
 export interface IssuerMatch {
-  issuer: IssuerMetaData,
+  issuer: IssuerMetaData | null,
   registry: Registry
+}
+
+export interface LookupResult {
+  matchingIssuers: IssuerMatch[],
+  uncheckedRegistries: Registry[]
 }
 
 const DID_MAP_REGISTRY_SPEC_V01 = '2.0.0'
@@ -55,35 +62,47 @@ export class RegistryClient {
    * 
    * @param registries - an array of registries to load
    */
+
+   // 'registries' will likely have been gotten with: fetch("https://github.com/digitalcredentials/known-registries/list.json");
+
    use ({ registries }: { registries: any }): void {
     this.registries = registries
-    }
+  }
   
 
-  async lookupIssuersFor (did: string): Promise<IssuerMatch[] | undefined> {
-      // this next fetch will be called by whoever calls this issuer-registry-client, and the result passed in on the
-  // 'use' method.
-//	const listOfKnownRegistries = fetch("https://github.com/digitalcredentials/known-registries/list.json");
-	// loop over all the registries, looking up the DID in each registry:
-	const matchingIssuers = await Promise.all(
-    // TODO: could use an accumulator here to filter out null entries.
+  async lookupIssuersFor (did: string): Promise<LookupResult> {
+  	// loop over all the registries, looking up the DID in each registry:
+  const allRegistryLookups = await Promise.all(
 		this.registries.map( async registry => {
       let issuer;
 			if (registry.type === 'oidf') {
-        // e.g., https://registry.dcconsortium.org/fetch?sub=
-				const response = await fetch(registry.fetchEndpoint + did)
-				issuer = await response.json();
+        try {
+				  const response = await fetch(registry.fetchEndpoint + did);
+				  issuer = await response.json();
+          registry.checked = true;
+        } catch (e) {
+          console.log(`error calling oidf endpoint: ${registry.fetchEndpoint}`)
+          console.log(e)
+        }
 				// maybe validate the JWT?
 				// likely transform the returned oidf entity statement into some simpler common form
 			} else if (registry.type === 'dcc-legacy') {
-				const response = await fetch(registry.url as string)
-				const listOfIssuersByDID = await response.json();
-				issuer = listOfIssuersByDID.registry[did]
+				try { 
+          const response = await fetch(registry.url as string);
+				  const listOfIssuersByDID = await response.json();
+          issuer = listOfIssuersByDID.registry[did];
+          registry.checked = true;
+        }  catch (e) {
+          console.log(`error calling oidf endpoint: ${registry.fetchEndpoint}`)
+          console.log(e)
+        }
+				
 			}
       return {issuer, registry}
 		})
 	)
-  // need to filter out nulls in this list:
-	return matchingIssuers
+  const matchingIssuers = allRegistryLookups.filter(lookup=>lookup.registry.checked && lookup.issuer)
+  const uncheckedRegistries = allRegistryLookups.filter(lookup=>!lookup.registry.checked).map(lookup=>lookup.registry)
+	return {matchingIssuers, uncheckedRegistries}
   }
 }
