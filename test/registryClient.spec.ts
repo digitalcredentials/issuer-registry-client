bbc2f2eceb27cf28f5a7b7ee43fd79fb080e66a2
James Chartrand <jc.chartrand@gmail.com>
2025-04-24 11:57:18 -0400

    fix oidf test

--- test/registryClient.spec.ts
+++ test/registryClient.spec.ts
@@ -3,7 +3,7 @@ import { RegistryClient } from '../src/index.js'
 import nock from 'nock'
 import oidfFetchNock from './fixtures/nocks/oidfFetchNock.js'
 import { knownRegistries } from './fixtures/knownRegistries.js'
-import { oidfFetchResponse } from './fixtures/oidfFetchResponse.js'
+import { singleOIDFResult } from './fixtures/lookupResults/singleOIDFResult.js'
 
 describe('registry client', () => {
   beforeEach(async () => {
@@ -18,8 +18,9 @@ describe('registry client', () => {
     oidfFetchNock()
     const client = new RegistryClient()
     client.use({ registries: knownRegistries })
-    const result = client.lookupIssuersFor('did:web:oneuni.testuni.edu')
-    expect(result).to.deep.equal(oidfFetchResponse)
+    const result = await client.lookupIssuersFor('did:web:oneuni.testuni.edu')
+    console.log(JSON.stringify(result,null,2))
+    expect(result).to.deep.equal(singleOIDFResult)
     // expect(entry?.inRegistries?.size).to.equal(2)
   })
 })
