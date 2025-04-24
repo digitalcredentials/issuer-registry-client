1793a75d6076a054f4052cff466fd2408457fda4
James Chartrand <jc.chartrand@gmail.com>
2025-04-24 11:57:18 -0400

    fix oidf test

--- test/fixtures/nocks/oidfFetchNock.ts
+++ test/fixtures/nocks/oidfFetchNock.ts
@@ -2,7 +2,7 @@ import nock from 'nock'
 import { oidfFetchResponse } from '../oidfFetchResponse.js'
 
 export default () => {
-  nock('http://registry.dcconsortium.org')
+  nock('https://registry.dcconsortium.org')
     .get('/fetch?sub=did:web:oneuni.testuni.edu')
     .reply(200, oidfFetchResponse)
 }
