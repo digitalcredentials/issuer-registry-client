9ea9abd297a9a51a327d93909590fc9b03c76561
James Chartrand <jc.chartrand@gmail.com>
2025-04-25 17:16:30 -0400

    transform legacy result to oidf format

--- test/fixtures/didLookupResults/doubleLegacyResult.ts
+++ test/fixtures/didLookupResults/doubleLegacyResult.ts
@@ -1,29 +1,33 @@
 export const doubleLegacyResult = {
-    "matchingIssuers": [
-      {
-        "issuer": {
-          "name": "(Example) My University",
-          "location": "Cambridge, MA, USA",
-          "url": "https://digitalcredentials.mit.edu"
-        },
-        "registry": {
-          "type": "dcc-legacy",
-          "name": "DCC Sandbox Registry",
-          "url": "https://digitalcredentials.github.io/sandbox-registry/registry.json"
+  "matchingIssuers": [
+    {
+      "issuer": {
+        federation_entity: {
+          organization_name: "(Example) My University",
+          homepage_uri: "https://digitalcredentials.mit.edu",
+          location: "Cambridge, MA, USA"
         }
       },
-      {
-        "issuer": {
-          "name": "My University",
-          "location": "Cambridge, MA, USA",
-          "url": "https://digitalcredentials.mit.edu"
-        },
-        "registry": {
-          "type": "dcc-legacy",
-          "name": "DCC Community Registry",
-          "url": "https://digitalcredentials.github.io/community-registry/registry.json"
-        }
+      "registry": {
+        "type": "dcc-legacy",
+        "name": "DCC Sandbox Registry",
+        "url": "https://digitalcredentials.github.io/sandbox-registry/registry.json"
       }
-    ],
-    "uncheckedRegistries": []
-  }
+    },
+    {
+      "issuer": {
+        federation_entity: {
+          organization_name: "My University",
+          "location": "Cambridge, MA, USA",
+          "homepage_uri": "https://digitalcredentials.mit.edu"
+        }
+      },
+      "registry": {
+        "type": "dcc-legacy",
+        "name": "DCC Community Registry",
+        "url": "https://digitalcredentials.github.io/community-registry/registry.json"
+      }
+    }
+  ],
+  "uncheckedRegistries": []
+}
