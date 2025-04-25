8630f11fd1af6894d828287b84eaa2239f024387
James Chartrand <jc.chartrand@gmail.com>
2025-04-25 13:40:22 -0400

    re-organize fixtures

--- test/registryClient.spec.ts
+++ test/registryClient.spec.ts
@@ -3,19 +3,10 @@ import { RegistryClient } from '../src/index.js'
 import nock from 'nock'
 import { singleOIDFNock, doubleLegacyNock } from './fixtures/nocks/oidfFetchNock.js'
 import { knownRegistries } from './fixtures/knownRegistries.js'
-import { singleOIDFResult } from './fixtures/lookupResults/singleOIDFResult.js'
-import { doubleLegacyResult } from './fixtures/lookupResults/doubleLegacyResult.js'
+import { singleOIDFResult } from './fixtures/didLookupResults/singleOIDFResult.js'
+import { doubleLegacyResult } from './fixtures/didLookupResults/doubleLegacyResult.js'
 
 describe('registry client', () => {
-
-  
-  beforeEach(async () => {
-    if (!nock.isActive()) nock.activate()
-  })
-
-  afterEach(async () => {
-    nock.restore()
-  }) 
 
   it('returns matching oidf result', async () => {
     singleOIDFNock()
@@ -23,16 +14,27 @@ describe('registry client', () => {
     client.use({ registries: knownRegistries })
     const result = await client.lookupIssuersFor('did:web:twotr.testschool.edu')
     expect(result).to.deep.equal(singleOIDFResult)
-    
   })
 
-  it('returns matching legacy result', async () => {
+  it('returns double matching legacy result', async () => {
     doubleLegacyNock()
     const client = new RegistryClient()
     client.use({ registries: knownRegistries })
     const result = await client.lookupIssuersFor('did:key:z6MkpLDL3RoAoMRTwTgo3rs39ZwssfaPKtGdZw7AGRN7CK4W')
     expect(result).to.deep.equal(doubleLegacyResult)
   })
+
+  it('returns legacy and oidf result', async () => {
+
+    /// TODO make some nocks for the legacy stuff so it does return a match for the twotr did.
+    doubleLegacyNock()
+    const client = new RegistryClient()
+    client.use({ registries: knownRegistries })
+    const result = await client.lookupIssuersFor('did:key:z6MkpLDL3RoAoMRTwTgo3rs39ZwssfaPKtGdZw7AGRN7CK4W')
+    expect(result).to.deep.equal(doubleLegacyResult)
+  })
+
+
 })
 
 /*
