ec50bf9c0969d9f486213a88331a4b213939cd22
James Chartrand <jc.chartrand@gmail.com>
2025-04-25 18:05:40 -0400

    fix linting

--- test/fixtures/nocks/legacyRegistryNocks.ts
+++ test/fixtures/nocks/legacyRegistryNocks.ts
@@ -2,15 +2,14 @@ import nock from 'nock'
 import communityRegistryResponse from '../httpResponses/legacyResponses/dcc-community-response.js'
 import sandboxRegistryResponse from '../httpResponses/legacyResponses/dcc-sandbox-response.js'
 
-export const sandboxRegistryNock = () => {
+export const sandboxRegistryNock = (): void => {
   nock('https://digitalcredentials.github.io')
     .get('/sandbox-registry/registry.json')
     .reply(200, sandboxRegistryResponse)
 }
 
-export const communityRegistryNock = () => {
+export const communityRegistryNock = (): void => {
   nock('https://digitalcredentials.github.io')
     .get('/community-registry/registry.json')
     .reply(200, communityRegistryResponse)
 }
-
